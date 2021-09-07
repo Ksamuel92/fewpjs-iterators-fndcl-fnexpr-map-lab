72ff140010c4c5240458632eaed09a4b5bf37a9a
Kyle Samuel <ksamuel92@gmail.com>
2021-09-07 10:51:36 -0400

    commit tree 9e212d7a9c712a9281e7e5f3e7d476e13c12c6f7

--- index.js
+++ index.js
@@ -13,5 +13,11 @@ const tutorials = [
 
 const titleCased = () => {
   // use the .map method on the tutorials to return a new array
-  return tutorials
+  return tutorials.map(title => {
+    const words = title.split(' '); // splits element of array into another array separated by spaces
+    const capitalizedWords =
+    words.map(word => word.charAt(0).toUpperCase() + word.slice(1)); //capitalies first letter, word.slice adds rest of letters to the word, otherwise would just return capitalized letter
+    const newTitle = capitalizedWords.join(' ');
+    return newTitle
+  })
 }
